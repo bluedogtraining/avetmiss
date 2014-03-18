b84ef5bdbe70323c415a287ac088e4d08bfed6dd
Ryan-Senn <ryan.senn.au@gmail.com>
2014-03-18 10:45:11 +1000

    formatting

--- tests/FieldTest.php
+++ tests/FieldTest.php
@@ -6,53 +6,6 @@ use Avetmiss\Fields\Field;
 class FieldTest extends TestCase
 {
 
-	// make, lenght, name, in should all return a Field object
-	public function testFluentBuilder()
-	{
-		$field = Field::make('any');
-		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
-
-		$field->lenght(10);
-		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
-
-		$field->name('foo');
-		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
-
-		$field->in(['foo', 'bar']);
-		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
-
-		$field->pad('x');
-		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
-	}
-
-
-	public function testInWithValidValue()
-	{
-		$field = Field::make('any')->name('foo')->lenght(10)->in(['bar']);
-		$field->setValue('bar');
-
-		$this->assertEquals('bar       ', $field->render());
-	}
-
-
-	/**
-	 * @expectedException \UnexpectedValueException
-	 */
-	public function testInWithInvalidValue()
-	{
-		$field = Field::make('any')->name('foo')->lenght(10)->in(['bar']);
-		$field->setValue('fiddy');
-	}
-
-
-	public function testPad()
-	{
-		$field = Field::make('any')->name('foo')->lenght(10)->pad('.');
-		$field->setValue('bar');
-
-		$this->assertEquals('.......bar', $field->render());
-	}
-
 
 	/**
 	 * @expectedException \InvalidArgumentException
@@ -101,5 +54,53 @@ class FieldTest extends TestCase
 	{
 		$field = Field::make('date')->name('foo')->lenght(8);
 		$field->setValue(02032014);
+	}
+	
+
+	// make, lenght, name, in, pad should all return a Field object
+	public function testFluentBuilder()
+	{
+		$field = Field::make('any');
+		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
+
+		$field->lenght(10);
+		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
+
+		$field->name('foo');
+		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
+
+		$field->in(['foo', 'bar']);
+		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
+
+		$field->pad('.');
+		$this->assertInstanceOf('Avetmiss\Fields\Field', $field);
+	}
+
+
+	public function testInWithValidValue()
+	{
+		$field = Field::make('any')->name('foo')->lenght(10)->in(['bar']);
+		$field->setValue('bar');
+
+		$this->assertEquals('bar       ', $field->render());
+	}
+
+
+	/**
+	 * @expectedException \UnexpectedValueException
+	 */
+	public function testInWithInvalidValue()
+	{
+		$field = Field::make('any')->name('foo')->lenght(10)->in(['bar']);
+		$field->setValue('fiddy');
+	}
+
+
+	public function testPad()
+	{
+		$field = Field::make('any')->name('foo')->lenght(10)->pad('.');
+		$field->setValue('bar');
+
+		$this->assertEquals('.......bar', $field->render());
 	}
 }

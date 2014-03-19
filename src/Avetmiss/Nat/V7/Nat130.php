6961b5317e6c2ed6580a6b86e284a675a3365fa8
Ryan-Senn <ryan.senn.au@gmail.com>
2014-03-19 16:19:25 +1000

    add Nat130

---
+++ src/Avetmiss/Nat/V7/Nat130.php
@@ -0,0 +1,21 @@
+<?php namespace Avetmiss\Nat\V7;
+
+use Avetmiss\Row;
+use Avetmiss\Fields\Field;
+
+
+class Nat130 extends Row
+{
+
+
+    public function __construct()
+    {
+        $this->addFields([
+            Field::make('any')->name('training_organisation_id')->lenght(10),
+            Field::make('any')->name('program_id')->lenght(10),
+            Field::make('any')->name('client_id')->lenght(10),
+            Field::make('numeric')Field->name('year_program_completed')->lenght(4),
+            Field::make('any')->name('issued_flag')->lenght(1),
+        ]);
+    }
+}

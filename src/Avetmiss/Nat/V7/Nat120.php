f9cb15e170befaef784a3af1016c8dd4c78d7981
Ryan-Senn <ryan.senn.au@gmail.com>
2014-04-17 14:59:52 +1000

    add pad on numeric fields

--- src/Avetmiss/Nat/V7/Nat120.php
+++ src/Avetmiss/Nat/V7/Nat120.php
@@ -19,7 +19,7 @@ class Nat120 extends Row
 			Field::make('date')->name('activity_end_date')->length(8),
 			Field::make('numeric')->name('delivery_mode_id')->length(2),
 			Field::make('numeric')->name('outcome_id_national')->length(2),
-			Field::make('numeric')->name('scheduled_hours')->length(4),
+			Field::make('numeric')->name('scheduled_hours')->length(4)->pad(0),
 			Field::make('numeric')->name('funding_source_national')->length(2),
 			Field::make('numeric')->name('commencing_program_id')->length(1),
 			Field::make('any')->name('training_contract_id')->length(10),
@@ -29,11 +29,11 @@ class Nat120 extends Row
 			Field::make('any')->name('specific_funding_id')->length(10),
 			Field::make('any')->name('outcome_id_training_organisation')->length(3),
 			Field::make('any')->name('funding_source_state_training_authority')->length(3),
-			Field::make('numeric')->name('client_tuition_fee')->length(4),
+			Field::make('numeric')->name('client_tuition_fee')->length(4)->pad(0),
 			Field::make('any')->name('fee_exemption_concession_type_id')->length(1),
 			Field::make('any')->name('purchasing_contract_id')->length(12),
 			Field::make('any')->name('purchasing_contract_schedule_id')->length(3),
-			Field::make('numeric')->name('hours_attended')->length(4),
+			Field::make('numeric')->name('hours_attended')->length(4)->pad(0),
 			Field::make('any')->name('associated_course_id')->length(10),
 		));
 	}

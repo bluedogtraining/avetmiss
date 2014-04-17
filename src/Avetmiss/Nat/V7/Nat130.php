35920bd741f25b6303fefb7558fc7aeb5ae7756e
Ryan-Senn <ryan.senn.au@gmail.com>
2014-04-17 12:24:56 +1000

    fix year

--- src/Avetmiss/Nat/V7/Nat130.php
+++ src/Avetmiss/Nat/V7/Nat130.php
@@ -14,7 +14,7 @@ class Nat130 extends Row
             Field::make('any')->name('training_organisation_id')->length(10),
             Field::make('any')->name('program_id')->length(10),
             Field::make('any')->name('client_id')->length(10),
-            Field::make('numeric')->name('year_program_completed')->length(4),
+            Field::make('numeric')->name('year_program_completed')->length(4)->pad('20'), // pad with 20 in case just two digits are sent (ex 13 instead of 2013)
             Field::make('any')->name('issued_flag')->length(1),
         ]);
     }

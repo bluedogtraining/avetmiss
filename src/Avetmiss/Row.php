9f7c184712df9644396b467f9e50988e4aaf8340
josh smith <josh-smith@scotch.bluedog.local>
2014-05-09 15:44:18 +1000

    Added PopulateFields to allow reverse engineering

--- src/Avetmiss/Row.php
+++ src/Avetmiss/Row.php
@@ -47,6 +47,23 @@ abstract class Row
 		return $this->fields[$name];
 	}
 
+	/*
+	 * returns a row populated from $rowData
+	 */
+	public function populateFields($rowData){
+		//clone the row so the clean one remains
+		$workingRow = $rowData;
+		if(!$this->fields){
+			throw new UnexistingFieldsException("Class ".get_called_class()." does not contain fields to populate.");
+		}else{
+			foreach($this->fields as $field){
+				$value = substr($workingRow,0,$field->getLength());
+				$workingRow = substr($workingRow, $field->getLength());
+				$field->setValue($value);
+			}
+			return $this;
+		}
+	}
 
 	/**
 	 *	Populate one of the field
